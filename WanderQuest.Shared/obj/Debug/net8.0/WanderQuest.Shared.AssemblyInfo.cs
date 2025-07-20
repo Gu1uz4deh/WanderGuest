cac9658ad4c6c7eaf01e9f0bc3a78a2cb335c418
Gu1uz4deh <tural.guluzada.i@gmail.com>
2025-07-20 15:01:27 +0400

    ChatNotificationAdded

--- WanderQuest.Shared/obj/Debug/net8.0/WanderQuest.Shared.AssemblyInfo.cs
+++ WanderQuest.Shared/obj/Debug/net8.0/WanderQuest.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WanderQuest.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+099cb4292d49c70839601dc8837221f8dcce205f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ff66f7d8548a51ddbcac86ba762905be643b6bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("WanderQuest.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WanderQuest.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
